25eaa8270a30095d08346335fb0463f6013ea51e
bala-amypo <bala@amypo.in>
2025-12-23 07:27:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.Employee;
+import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
