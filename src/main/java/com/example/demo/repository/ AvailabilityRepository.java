a3c515d7b145991bf20050d2ca4da2e7fcbd1220
bala-amypo <bala@amypo.in>
2025-12-23 04:16:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/ AvailabilityRepository.java
+++ src/main/java/com/example/demo/repository/ AvailabilityRepository.java
@@ -2,10 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.EmployeeAvailability;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.time.LocalDate;
 import java.util.List;
 
 public interface AvailabilityRepository extends JpaRepository<EmployeeAvailability, Long> {
-    // This naming is critical for the "find symbol" error
-    List<EmployeeAvailability> findByAvailableDateAndAvailable(LocalDate date, boolean available);
+    List<EmployeeAvailability> findByEmployeeId(Long employeeId);
 }
